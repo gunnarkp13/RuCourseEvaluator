07329c3b7eb7234746289b6723a3bda6b73be35d
Guðjón Pétursson <gudjon12@ru.is>
2015-03-17 18:25:29 +0000

    admincontroller með vesen i ad birta ein template

--- src/JS/controllers/adminTemplateController.js
+++ src/JS/controllers/adminTemplateController.js
@@ -10,16 +10,46 @@ angular.module('RuCourseEvaluator').controller("adminTemplateController",[
 	'serverResource',
 	'sessionCookie', 
 	function ($scope, $location, $rootScope, $http, $routeParams, evaluationServer, serverResource, sessionCookie) {
-		var tempObj = $routeParams.tempObj;
-		$scope.questions = [];
+		
+		var tempID = $routeParams.tempObj;
+		$scope.teacherQuestions = [];
+		$scope.courseQuestions = [];
 		$scope.templateID = '';
 		$scope.TemplateTitle = '';
 		$scope.TemplateTitleEN = '';
+		$scope.introText = '';
+		$scope.introTextEN = '';
+		
+		console.log("nanan");
+		console.log(Object.keys($routeParams.tempObj));
 
-		if($routeParams.tempObj !== undefined) {
-			$scope.templateID = tempObj.templateID;
-			$scope.TemplateTitle = tempObj.TemplateTitle;
-			$scope.TemplateTitleEN = tempObj.TemplateTitleEN;		
-		}
+		
+
+		if(tempID !== undefined ) {
+			serverResource.getTemplate(tempID, sessionCookie.getToken)
+			.success(function (response) {
+				$scope.templateID = response.ID;
+				$scope.TemplateTitle = response.Title;
+				$scope.TemplateTitleEN = response.TitleEN;
+				$scope.teacherQuestions = response.TeacherQuestions;
+				$scope.courseQuestions = response.CourseQuestions;
+				$scope.introText = response.IntroText;
+				$scope.introTextEN = response.IntroTextEN;
+			});
+
+			
+			console.log("gott");
+			console.log($scope.TemplateTitle);
+		}	
+
+		$scope.addQuestion = function(sType, qType ) {
+
+		};
+
+		$scope.deletQuestion = function (sType, qId) {
+
+		};
+
+		
 	}
 ]);
