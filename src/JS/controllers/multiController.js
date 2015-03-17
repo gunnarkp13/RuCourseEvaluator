d2965d604d2875da6b781140381bd20f4874c616
Her Gunnar <gunnarkp13@ru.is>
2015-03-17 09:18:03 +0000

    strict

--- src/JS/controllers/multiController.js
+++ src/JS/controllers/multiController.js
@@ -1,3 +1,5 @@
+"use strict";
+
 angular.module('RuCourseEvaluator').controller('multiController', [ 
   '$scope', 
   function ($scope) { 
--- src/JS/controllers/singleController.js
+++ src/JS/controllers/singleController.js
@@ -1,3 +1,5 @@
+"use strict";
+
 angular.module('RuCourseEvaluator').controller('singleController', [ 
   '$scope', 
   function ($scope) { 
