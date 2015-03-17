ae827cba2f4e2fbdac3fb60fb9b1b6b72df00e18
GunniKalli <gunnarkp13@ru.is>
2015-03-17 11:21:59 +0000

    rosa

--- src/JS/controllers/adminTemplateListController.js
+++ src/JS/controllers/adminTemplateListController.js
@@ -8,12 +8,12 @@ angular.module('RuCourseEvaluator').controller("adminTemplateListController", [
 	'serverResource',
 	'sessionCookie',
 	function ($scope, $location, $http, evaluationServer, serverResource, sessionCookie) {
-		$scope.templist = [];
+		$scope.tempList = [];
 
 		if (sessionCookie.getRole() === 'admin') {
 			serverResource.getTemplates(sessionCookie.getToken())
 			.success(function (response) {
-				$scope.templist = response;
+				$scope.tempList = response;
 				console.log("success");
 				console.log(response);	
 			})
