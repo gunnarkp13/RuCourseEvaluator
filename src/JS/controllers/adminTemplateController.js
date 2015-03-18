e2a45e8a5400e5ca6000a80f31af0b86b28aa042
Her Gunnar <gunnarkp13@ru.is>
2015-03-18 01:30:58 +0000

    allskonar

--- src/JS/controllers/adminTemplateController.js
+++ src/JS/controllers/adminTemplateController.js
@@ -34,9 +34,53 @@ angular.module('RuCourseEvaluator').controller("adminTemplateController",[
 			.error(function (response) {
 				console.log("error");
 			});		
-		}	
-		$scope.addQuestion = function(sType, qType ) {
+		}
 
+		$scope.addQuestion = function (sType, qType) {
+			var qId = 0;
+			if (sType === 'course') {
+				qId = $scope.courseQuestions.size();
+			} else {
+				qId = $scope.teacherQuestions.size();
+			}
+
+			var tmpQ = {
+				"ID": qId,
+				"Text": '',
+				"TextEN": '',
+				"ImageURL": '',
+				"Type": '',
+				"Answers": []
+			};
+
+			if (sType === 'course') {
+				$scope.courseQuestions.push(tmpQ);
+			} else {
+				$scope.teacherQuestions.push(tmpQ);
+			}			
+		};
+
+		$scope.addAnswer = function (sType, qId) {
+			var aId = 0;
+			if (sType === 'course') {
+				aId = $scope.courseQuestions[$scope.courseQuestions.size() - 1].Answers.size();
+			} else {
+				aId = $scope.teacherQuestions[$scope.teacherQuestions.size() - 1].Answers.size();
+			}
+			 
+			var answer = {
+				"ID": aId,
+				"Text": '',
+				"TextEN": '',
+				"ImageURL": '',
+				"Weight": ''
+			};
+
+			if (sType === 'course') {
+				$scope.courseQuestions[qId].Answers.push(answer);
+			} else {
+				$scope.teacherQuestions[qId].Answers.push(answer);
+			}
 		};
 
 		$scope.deletQuestion = function (sType, qId) {
