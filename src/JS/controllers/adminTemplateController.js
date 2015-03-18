42e2e344e1b7d2c7ac2401b738e3ac9efc37186f
Her Gunnar <gunnarkp13@ru.is>
2015-03-18 19:16:23 +0000

    get postað template

--- src/JS/controllers/adminTemplateController.js
+++ src/JS/controllers/adminTemplateController.js
@@ -18,21 +18,23 @@ angular.module('RuCourseEvaluator').controller("adminTemplateController",[
 		$scope.TemplateTitleEN = '';
 		$scope.introText = '';
 		$scope.introTextEN = '';
+		$scope.imageURL = '';
 
 		if($routeParams.tempObj !== undefined ) {
 			serverResource.getTemplate($routeParams.tempObj, sessionCookie.getToken())
 			.success(function (response) {
-				console.log(Object.keys(response));
+				console.log("success");
+				$scope.imageURL = response.ImageURL; 
 				$scope.templateID = response.ID;
 				$scope.TemplateTitle = response.Title;
 				$scope.TemplateTitleEN = response.TitleEN;
 				console.log(response['TeacherQuestions']);
 				for (var tQ in response['TeacherQuestions']) {
-					console.log(response['TeacherQuestions'][tQ]);
+					//console.log(response['TeacherQuestions'][tQ]);
 					$scope.teacherQuestions.push(response['TeacherQuestions'][tQ]);
 				}
 				for (var cQ in response['CourseQuestions']) {
-					console.log(cQ);
+					//console.log(cQ);
 					$scope.courseQuestions.push(response['CourseQuestions'][cQ]);
 				}
 				$scope.introText = response.IntroText;
@@ -56,7 +58,7 @@ angular.module('RuCourseEvaluator').controller("adminTemplateController",[
 				"Text": '',
 				"TextEN": '',
 				"ImageURL": '',
-				"Type": '',
+				"Type": qType,
 				"Answers": []
 			};
 
@@ -68,11 +70,23 @@ angular.module('RuCourseEvaluator').controller("adminTemplateController",[
 		};
 
 		$scope.addAnswer = function (sType, qId) {
+			console.log(qId);
+			console.log(sType);
+			console.log($scope.courseQuestions);
+			console.log(Object.keys($scope.courseQuestions));
 			var aId = 0;
-			if (sType === 'course') {
-				aId = $scope.courseQuestions[$scope.courseQuestions.length - 1].Answers.length;
+			if (sType === 'Course') {
+				if($scope.courseQuestions.length <= 0){
+					aId = 0;
+				} else {
+					aId = $scope.courseQuestions[$scope.courseQuestions.length - 1].Answers.length;
+				}
 			} else {
-				aId = $scope.teacherQuestions[$scope.teacherQuestions.length - 1].Answers.length;
+				if($scope.teacherQuestions.length <= 0){
+					aId = 0;
+				} else {
+					aId = $scope.teacherQuestions[$scope.teacherQuestions.length - 1].Answers.length;
+				}
 			}
 			 
 			var answer = {
@@ -83,15 +97,48 @@ angular.module('RuCourseEvaluator').controller("adminTemplateController",[
 				"Weight": ''
 			};
 
-			if (sType === 'course') {
-				$scope.courseQuestions[qId].Answers.push(answer);
+			if (sType === 'Course') {
+				var index1 = _.findIndex($scope.courseQuestions, {ID:qId}); 
+				$scope.courseQuestions[index1].Answers.push(answer);
 			} else {
-				$scope.teacherQuestions[qId].Answers.push(answer);
+				var index2 = _.findIndex($scope.teacherQuestions, {ID:qId});
+				$scope.teacherQuestions[index2].Answers.push(answer);
 			}
 		};
 
 		$scope.deletQuestion = function (sType, qId) {
 
+		};
+
+		$scope.submit = function () {
+			var template = {
+				'ID': 					999, 
+				'Title': 				$scope.TemplateTitle,
+				'TitleEN': 				$scope.TemplateTitleEN,
+				'ImageURL': 			$scope.imageURL,
+				'IntroText': 			$scope.introText,
+				'IntroTextEN': 			$scope.introTextEN,
+				'CourseQuestions': 		$scope.courseQuestions,
+				'TeacherQuestions': 	$scope.teacherQuestions
+			};
+
+			for (var x in $scope.teacherQuestions) {
+				console.log(x);
+			}
+
+			for (var y in $scope.courseQuestions) {
+				console.log(y);
+			}
+
+			serverResource.postTemplate(template, sessionCookie.getToken())
+			.success( function (response) {
+				console.log("bingo");
+				console.log(response);
+			})
+			.error(function (response) {
+				console.log("andskotinn !");
+				console.log(response);
+			});
 		};		
 	}
 ]);
